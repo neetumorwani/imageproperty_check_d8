c4dc9ef3d78f52f9552468203a3835eddbdb8040
Neetu Morwani <neetumorwani@qed42.com>
2014-11-26 17:11:03 +0530

    RUN CRON form built + logic started

--- src/Controller/imagepropertyCheckController.php
+++ src/Controller/imagepropertyCheckController.php
@@ -8,6 +8,7 @@
 namespace Drupal\imageproperty_check\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Form\FormBuilder;
 use Drupal\Core\Url;
 
 
@@ -16,10 +17,19 @@ class imagepropertyCheckController extends ControllerBase {
 
   public function __construct() {
     $this->database = \Drupal::database();
+    $this->imageproperty_check_config = \Drupal::config('imageproperty_check.settings');
 
   }
 
   public function imagepropertyCheckReports() {
+    $form = \Drupal::formBuilder()->getForm('Drupal\imageproperty_check\Form\ImagepropertyCheckRunCron');
+    $list_image_style = image_style_options();
+    unset($list_image_style['']);
+    foreach ($list_image_style as $key => $value) {
+      $images = file_scan_directory('public://styles/' . $key, '/.*/');
+      $imageproperty_check_value = $this->imageproperty_check_config->get('imageproperty_check_type_'.$key);
+      dsm($imageproperty_check_value);
+    }
     return array(
       '#theme' => 'item_list',
       '#items' => array(1, 2),
