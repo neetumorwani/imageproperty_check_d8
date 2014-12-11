65560cb9d067cb55d93b1a4741e904e55dc8f4bb
Neetu Morwani <neetumorwani@qed42.com>
2014-12-11 18:07:32 +0530

    run cron manually form element added

--- src/Form/ImagepropertyCheckUpdateReport.php
+++ src/Form/ImagepropertyCheckUpdateReport.php
@@ -21,11 +21,16 @@ class ImagepropertyCheckUpdateReport extends FormBase {
     $form = array();
     $form['actions']['update_report'] = array(
       '#type' => 'submit',
-      '#value' => t('Update Image size and Aspect Ratio Report'),
+      '#value' => t('Update Image Aspect Ratio Report'),
+    );
+    $form['actions']['update_image_size_report'] = array(
+      '#type' => 'button',
+      '#value' => t('Update Image Size Report'),
+      '#executes_submit_callback' => array(TRUE)  ,
     );
     $form['actions']['run_cron_manually'] = array(
       '#type' => 'button',
-      '#value' => t('Run cron to recieve emails regarding images with glitches'),
+      '#value' => t('Run Cron manually to recieve email update regarding images'),
       '#executes_submit_callback' => array(TRUE)  ,
     );
     return $form;
@@ -34,7 +39,7 @@ class ImagepropertyCheckUpdateReport extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $input = $form_state->getUserInput();
     $button_clicked = $input['op'];
-    if($button_clicked === 'Run cron to recieve emails regarding images with glitches') {
+    if($button_clicked === 'Update Image Size Report') {
       $imagepropertyCheckController = new \Drupal\imageproperty_check\Controller\imagepropertyCheckController();
       $imagepropertyCheckController->imagepropertyCheckReports();
     }
